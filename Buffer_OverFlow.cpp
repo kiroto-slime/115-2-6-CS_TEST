cb35f97293672504e6bf4884124ceb8c939e13a5
Quartz Slimer <edward23242441@gmail.com>
2026-02-05 17:38:23 +0800

    Questions 1

---
+++ Buffer_OverFlow.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int pass = 0 ;
+    char buffer[10];
+    cin >> buffer ;
+
+    if (pass == 0x41){
+        cout << "Congratulations, you have scored 50 points!" << endl;
+    } else {
+        cout << "Try again!" << endl;
+    }
+}
